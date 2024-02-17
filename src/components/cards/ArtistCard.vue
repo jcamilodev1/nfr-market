0545f61e722a21c347fa08ee038cc7193a021dd0
jcamilodev2 <123014168+jcamilodev2@users.noreply.github.com>
2024-02-17 17:39:26 -0500

    top Artist

---
+++ src/components/cards/ArtistCard.vue
@@ -0,0 +1,51 @@
+<template>
+   <article class="artistCard">
+        <img src="@/assets/images/ArtistAvatar.png" alt="">
+        <h2>Keepitreal</h2>
+        <p><span>Total Sales: </span> 34.53 ETH</p>
+        <div>
+          1
+        </div>
+      </article>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+.artistCard{
+  display: grid;
+  background-color: #3B3B3B;
+  border-radius: var(--radius);
+  padding: 20px;
+  justify-items: center;
+  min-width: 250px;
+  position: relative;
+  h2{
+    font-size: 25px;
+    margin: 15px 0;
+  }
+  p{
+    font-weight: 300;
+    font-size: 15px;
+    span{
+      color: #858584;
+    }
+  }
+  div{
+    position: absolute;
+    top: 15px;
+    left: 15px;
+
+    background: #2B2B2B;
+    border-radius: 100%;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
